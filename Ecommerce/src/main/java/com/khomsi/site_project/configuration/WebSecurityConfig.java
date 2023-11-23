21739346aa828ecc94a2d6457761f3f9b0d2d0c9
moon <110921861+sumaiyaafrinmoon@users.noreply.github.com>
2023-11-23 17:28:41 +0600

    its simple ecommerce site

---
+++ Ecommerce/src/main/java/com/khomsi/site_project/configuration/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.khomsi.site_project.configuration;
+
+import com.khomsi.site_project.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests().antMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
+                .antMatchers("/js/**", "/css/**", "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().and().exceptionHandling()
+                .accessDeniedPage("/error403");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder)
+                .usersByUsernameQuery("select login, password, 'true' as enabled from user where login=?")
+                .authoritiesByUsernameQuery("select login, role from user where login=?");
+    }
+
+}
+
